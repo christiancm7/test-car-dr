055a6421cf2c426e344a1481cb8147812ac3ff46
christian morales <christianmorales@christians-mbp.mynetworksettings.com>
2024-02-20 10:20:24 -0600

    first commit

---
+++ ios/CarDrSDK.framework/Headers/CarDrSDK.h
@@ -0,0 +1,18 @@
+//
+//  CarDrSDK.h
+//  CarDrSDK
+//
+//  Created by Arvind Mehta on 21/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarDrSDK.
+FOUNDATION_EXPORT double CarDrSDKVersionNumber;
+
+//! Project version string for CarDrSDK.
+FOUNDATION_EXPORT const unsigned char CarDrSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarDrSDK/PublicHeader.h>
+
+
